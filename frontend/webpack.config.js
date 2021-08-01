a8fcfdd257650d94d6338846f02b370f320178a7
Kevin Zhang <kazp058@gmail.com>
2021-07-31 23:08:34 -0500

    Integracion exitosa entre React y Django.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify("production"),
+        NODE_ENV: JSON.stringify("development"),
       },
     }),
   ],
