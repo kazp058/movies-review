a2d93ea0a5ebc237db7fcc816b7ee2049a50cf5c
Kevin Zhang <kazp058@gmail.com>
2021-07-31 21:10:19 -0500

    Test de MySQL y Django

--- api/views.py
+++ api/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
 
 # Create your views here.
 
 def main(request):
-    return HttpResponse("Hello")
+    cursor = connection.cursor()
+    cursor.execute("SELECT count(*) FROM app.texts")
+    r = cursor.fetchone()
+    return HttpResponse(r)
+
+def product_list(request):
+    query = "SELECT * FROM products"
